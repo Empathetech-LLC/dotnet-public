7470e59ce01b9bf1d2b5d5331995853ce8eb980b
empathictech <105748722+empathictech@users.noreply.github.com>
2023-11-13 22:12:02 -0500

    Updates from version 2.2.1 (#15)

--- lib/utils/CharityOrgs.dart
+++ lib/utils/CharityOrgs.dart
@@ -39,110 +39,124 @@ class CharityOrgs extends StatelessWidget {
     bool limitedSpace = ScreenSpace.of(context)?.isLimited ?? false;
 
     final double buttonSpacer = EzConfig.instance.prefs[buttonSpacingKey];
+    final double textSpacer = EzConfig.instance.prefs[textSpacingKey];
 
     double twoThirdsWidth = widthOf(context) * (2 / 3);
     double sharedColWidth = (widthOf(context) - 2 * buttonSpacer) * 0.3;
     double circleDiameter =
         2.0 * CircleAvatarRadius * sqrt(MediaQuery.of(context).textScaleFactor);
 
-    return EzRowCol(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        // AnitaB.org
-        ConstrainedBox(
-          constraints: BoxConstraints(
-            maxWidth: limitedSpace ? twoThirdsWidth : sharedColWidth,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              logoImage(
-                width: circleDiameter,
-                url: anitaBorgLink,
-                assetPath: anitaBorgIconPath,
-                semanticLabel: Lang.of(context)!.gAnitaBorgIconHint,
-              ),
-              EzSpacer(buttonSpacer),
-              EzLink(
-                'AnitaB.org',
-                style: titleLinkStyle,
-                url: Uri.parse(anitaBorgCNavLink),
-                semanticsLabel: Lang.of(context)!.fpsAnitaCNavHint,
-              ),
-              EzSpacer(buttonSpacer),
-              EzText(
-                Lang.of(context)!.fpsAnitaMission,
-                style: contentStyle,
-              ),
-            ],
-          ),
-        ),
-        EzSwapSpacer(buttonSpacer),
+    final List<Widget> anitaB = [
+      logoImage(
+        width: circleDiameter,
+        url: anitaBorgLink,
+        assetPath: anitaBorgIconPath,
+        semanticLabel: Lang.of(context)!.gAnitaBorgIconHint,
+      ),
+      EzSpacer(buttonSpacer),
+      EzLink(
+        'AnitaB.org',
+        style: titleLinkStyle,
+        url: Uri.parse(anitaBorgCNavLink),
+        semanticsLabel: Lang.of(context)!.fpsAnitaCNavHint,
+      ),
+      EzSpacer(buttonSpacer),
+      EzText(
+        Lang.of(context)!.fpsAnitaMission,
+        style: contentStyle,
+      ),
+    ];
 
-        // code.org
-        ConstrainedBox(
-          constraints: BoxConstraints(
-            maxWidth: limitedSpace ? twoThirdsWidth : sharedColWidth,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              logoImage(
-                width: circleDiameter,
-                url: codeDotOrgLink,
-                assetPath: codeDotOrgIconPath,
-                semanticLabel: Lang.of(context)!.gCodeDotOrgIconHint,
-              ),
-              EzSpacer(buttonSpacer),
-              EzLink(
-                'code.org',
-                style: titleLinkStyle,
-                url: Uri.parse(codeDotOrgCNavLink),
-                semanticsLabel: Lang.of(context)!.fpsCodeCNavHint,
-              ),
-              EzSpacer(buttonSpacer),
-              EzText(
-                Lang.of(context)!.fpsCodeMission,
-                style: contentStyle,
-              ),
-            ],
-          ),
-        ),
-        EzSwapSpacer(buttonSpacer),
+    final List<Widget> codeOrg = [
+      logoImage(
+        width: circleDiameter,
+        url: codeDotOrgLink,
+        assetPath: codeDotOrgIconPath,
+        semanticLabel: Lang.of(context)!.gCodeDotOrgIconHint,
+      ),
+      EzSpacer(buttonSpacer),
+      EzLink(
+        'code.org',
+        style: titleLinkStyle,
+        url: Uri.parse(codeDotOrgCNavLink),
+        semanticsLabel: Lang.of(context)!.fpsCodeCNavHint,
+      ),
+      EzSpacer(buttonSpacer),
+      EzText(
+        Lang.of(context)!.fpsCodeMission,
+        style: contentStyle,
+      ),
+    ];
 
-        // World Savvy
-        ConstrainedBox(
-          constraints: BoxConstraints(
-            maxWidth: limitedSpace ? twoThirdsWidth : sharedColWidth,
-          ),
-          child: Column(
+    final List<Widget> worldSavvy = [
+      logoImage(
+        width: circleDiameter,
+        url: worldSavvyLink,
+        assetPath: worldSavvyIconPath,
+        semanticLabel: Lang.of(context)!.gWorldSavvyIconHint,
+        color: Colors.transparent,
+      ),
+      EzSpacer(buttonSpacer),
+      EzLink(
+        'World Savvy',
+        style: titleLinkStyle,
+        url: Uri.parse(worldSavvyCNavLink),
+        semanticsLabel: Lang.of(context)!.fpsSavvyCNavHint,
+      ),
+      EzSpacer(buttonSpacer),
+      EzText(
+        Lang.of(context)!.fpsSavvyMission,
+        style: contentStyle,
+      ),
+    ];
+
+    return limitedSpace
+        ? ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: twoThirdsWidth),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ...anitaB,
+                EzSpacer(textSpacer),
+                ...codeOrg,
+                EzSpacer(textSpacer),
+                ...worldSavvy,
+              ],
+            ),
+          )
+        : Row(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              logoImage(
-                width: circleDiameter,
-                url: worldSavvyLink,
-                assetPath: worldSavvyIconPath,
-                semanticLabel: Lang.of(context)!.gWorldSavvyIconHint,
-                color: Colors.transparent,
+              // AnitaB.org
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: sharedColWidth),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: anitaB,
+                ),
               ),
-              EzSpacer(buttonSpacer),
-              EzLink(
-                'World Savvy',
-                style: titleLinkStyle,
-                url: Uri.parse(worldSavvyCNavLink),
-                semanticsLabel: Lang.of(context)!.fpsSavvyCNavHint,
+              EzSpacer.row(buttonSpacer),
+
+              // Code.org
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: sharedColWidth),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: codeOrg,
+                ),
               ),
-              EzSpacer(buttonSpacer),
-              EzText(
-                Lang.of(context)!.fpsSavvyMission,
-                style: contentStyle,
+              EzSpacer.row(buttonSpacer),
+
+              // World Savvy
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: sharedColWidth),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: worldSavvy,
+                ),
               ),
             ],
-          ),
-        ),
-        EzSwapSpacer(buttonSpacer),
-      ],
-    );
+          );
   }
 }
--- lib/utils/finances.dart
+++ lib/utils/finances.dart
@@ -3,8 +3,8 @@ import '../utils/utils.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/services.dart';
 
-/// Display a double [value] as a United States Dollar amount
-/// Auto formats decminals, comma separators, and $ symbol
+/// Display double [value] as a United States Dollar amount
+/// Auto formats decimals, comma separators, and $ symbol
 String asUSD(double value) {
   return NumberFormat.currency(locale: 'en_US', symbol: '\$').format(value);
 }
@@ -16,36 +16,53 @@ Future<double> calculateGoal() async {
   return _gatherSalaries() + await _gatherExpenses();
 }
 
-/// Team member's hours * [_comfortableWage]
+/// The sum of each member's annual hours * [_comfortableWage]
 double _gatherSalaries() {
-  return hours.values.fold(
+  final double wage = _comfortableWage();
+
+  return annualHours.values.fold(
     0.0,
-    (sum, value) => sum + (value * _comfortableWage()),
+    (sum, hours) => sum + (hours * wage),
   );
 }
 
-/// Team member hours
+/// Annual hours for each Empathetech member
 /// Full time == 40 * 52 == 2080
-const Map<String, double> hours = {"Michael Waldron": 2080};
+const Map<String, double> annualHours = {"Michael Waldron": 2080};
 
-/// Eventually, Empathetech will create its own comfortable wage calculator
-/// For now, we will rely on adapting MIT's living wage calculator: https://livingwage.mit.edu/
-/// Their methodology: https://livingwage.mit.edu/pages/methodology
-/// Our goal: For Empathetech employees to be able to live a comfortable life anywhere in the Country of Domicile
+/// Eventually, Empathetech will create its own comfortable wage calculator.
+/// Currently, we rely on adapting MIT's living wage calculator.
+///
+/// https://livingwage.mit.edu/
+/// https://livingwage.mit.edu/pages/methodology
+///
+/// The Living Wage team asks "that users do not scrape the data".
+/// So, all [_comfortableWage] values are hard-coded and updated manually.
+/// Last validated: Q4 2023
+///
+/// Our goal:
+///   For Empathetech employees to be able to live a comfortable life anywhere in the Country of Domicile.
+///
 /// Our methodology:
-/// 1. Take the most expensive jurisdiction (NY, NY)
-/// 2. Use the single adult table
-/// 3. Use the living wage row
-/// 4. Use the 3 children column to emulate a comfortable life...
-///    Child 1: Caring for a dependent (actual child, pet, etc.)
-///    Child 2: Going on vacations
-///    Child 3: Having hobbies
-/// 5. Value as of Q3 2023: $75.43/hour
+///   1.  Take the most expensive county (thus satisfying: "anywhere"):
+///         Santa Clara Country, California; https://livingwage.mit.edu/counties/06085
+///   2.  Use the 1 (Single) Adult table.
+///   3.  Use the Living Wage row.
+///   4.  The Living Wage data encompasses families with up to 3 children.
+///       Use this column to emulate a comfortable wage, accounting for comfort concepts un-included in basic needs...
+///         - Caring for dependents other than children
+///         - Having hobbies
+///         - Going on vacations
+///         - Accruing  wealth so that you can retire earlier than the average of only 12 years before you die (in the US)
+///         - Providing all of the above to your children as well
+///   5.  The current value: $92.12/hour
+///   5b. P.S. If the Living Wage team at MIT adds larger families, Empathetech will remain at 3 until more internal research is completed (&& shared).
 double _comfortableWage() {
-  return 75.43;
+  return 92.12;
 }
 
 /// Sum the totals from the latest YTD expense report
+/// See them here: https://github.com/Empathetech-LLC/expense-report
 Future<double> _gatherExpenses() async {
   final String csvData = await rootBundle.loadString(ytdReportPath);
   final List<String> lines = csvData.split('\n');
@@ -64,13 +81,15 @@ Future<double> _gatherExpenses() async {
   return sum;
 }
 
-/// Empathetech's current revenue streams...
+/// Empathetech's current revenue streams are...
 /// Contracts and donations
 double calculateIncome() {
   return _gatherContracts() + _gatherDonations();
 }
 
-/// Empathetech has yet to recieve income from any contracts
+/// For prospective contractees: our rates follow a similar methodology to [_comfortableWage]
+/// However, we adjust for whatever county you live in.
+/// If you're international: we'll work it out. There's probably a cost of living converter for that.
 double _gatherContracts() {
   return 0;
 }
